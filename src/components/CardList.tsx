0f162793c5320c2e34b625d8d771c1035c854e43
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-05 10:17:54 -0300

    Fix CardList

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, useDisclosure, Box, Image, Heading, Text } from '@chakra-ui/react';
+import { SimpleGrid, useDisclosure} from '@chakra-ui/react';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -16,18 +16,21 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  const { isOpen, onClose } = useDisclosure();
+  const { isOpen, onClose, onOpen } = useDisclosure();
   const [ imgUrl, setImageUrl] = useState('');
   
   function openModal(url) {
     setImageUrl(url)
+    onOpen();
   }
 
   return (
     <>
-      <SimpleGrid columns={3} spacing={10}>
+      <SimpleGrid columns={3} spacing='40px'>
         {cards.map(card => {
-          <Card key={card.id} data={card} viewImage={() => openModal(card.url)}/>
+          return (
+            <Card key={card.id} data={card} viewImage={() => openModal(card.url)}/>
+          )
         })}
       </SimpleGrid>
 
